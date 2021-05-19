c89a6c36036eba96eefd679784aabfacf9aca4d7
Emmanuel <emmanuel.lazarte.012@gmail.com>
2021-05-19 14:45:39 -0300

    Primer commit..! :)

---
+++ procesos/src/capturar-signal2.c
@@ -0,0 +1,24 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+// estas macros son solo de referencia
+//
+#define SIGHUP  1   /* Hangup the process */
+#define SIGINT  2   /* Interrupt the process */
+#define SIGQUIT 3   /* Quit the process */
+#define SIGILL  4   /* Illegal instruction. */
+#define SIGTRAP 5   /* Trace trap. */
+#define SIGABRT 6   /* Abort. */
+
+void sigint();
+
+int main(void){
+
+
+
+  return 0;
+}
